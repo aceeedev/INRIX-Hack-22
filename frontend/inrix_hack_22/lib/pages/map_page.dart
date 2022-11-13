8976e0adb062481f1ca5cbf6c8cfa81a7c791d44
leochen <Leo_Chen_zy@hotmail.com>
2022-11-12 21:23:02 -0800

    updated polygon on map page

--- frontend/inrix_hack_22/lib/pages/map_page.dart
+++ frontend/inrix_hack_22/lib/pages/map_page.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -15,6 +16,37 @@ class _MapPageState extends State<MapPage> {
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  // created list of locations to display polygon
+  List<LatLng> points = [
+    LatLng(37.350264, -121.943206),
+    LatLng(37.351582, -121.939321),
+    LatLng(37.348064, -121.937111),
+    LatLng(37.346617, -121.941263),
+  ];
+
+  Set<Polygon> _polygon = HashSet<Polygon>();
+
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    //initialize polygon
+    _polygon.add(Polygon(
+      // given polygonId
+      polygonId: PolygonId('1'),
+      // initialize the list of points to display polygon
+      points: points,
+      // given color to polygon
+      fillColor: Colors.green.withOpacity(0.3),
+      // given border color to polygon
+      strokeColor: Colors.green,
+      geodesic: true,
+      // given width of border
+      strokeWidth: 4,
+    ));
   }
 
   @override
