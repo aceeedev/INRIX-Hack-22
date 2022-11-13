0f0e944603112afe69d58e8fdef642035f425baf
Andrew Collins <56804602+aceeedev@users.noreply.github.com>
2022-11-12 21:00:45 -0800

    Updated Location Page

--- frontend/inrix_hack_22/lib/pages/home_page.dart
+++ frontend/inrix_hack_22/lib/pages/home_page.dart
@@ -2,12 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:inrix_hack_22/models/proximity_reminder.dart';
 import 'package:inrix_hack_22/pages/form_page.dart';
+import 'package:inrix_hack_22/pages/location_page.dart';
 import 'package:inrix_hack_22/backend/database_manager.dart';
-import 'package:inrix_hack_22/pages/map_page.dart';
-import 'package:inrix_hack_22/backend/geolocation.dart' as globals;
-//import 'package:inrix_hack_22/pages/map_page.dart';
-//import 'package:inrix_hack_22/globals.dart' as globals;
-//import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -52,34 +48,19 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    late GoogleMapController mapController;
-
-    final LatLng _center = const LatLng(45.521563, -122.677433);
-
-    void _onMapCreated(GoogleMapController controller) {
-      mapController = controller;
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
+      body: Center(
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : proximityReminders.isEmpty
+                ? const Text(
+                    'Add a location to start!',
+                  )
+                : buildProximityReminders(),
       ),
-      // Center(
-      //   child: isLoading
-      //       ? CircularProgressIndicator()
-      //       : proximityReminders.isEmpty
-      //           ? Text(
-      //               globals.homeLocation,
-      //             )
-      //           : buildProximityReminders(),
-      // ),
       floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
           onPressed: () async {
@@ -95,33 +76,42 @@ class _HomePageState extends State<HomePage> {
       itemBuilder: (context, index) {
         ProximityReminder proximityReminder = proximityReminders[index];
 
-        return Card(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-              proximityReminder.phoneNumberName,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10.0),
-              child: Text(
-                'Phone: ${proximityReminder.phoneNumber}',
+        return GestureDetector(
+          onTap: () async {
+            await Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) =>
+                  LocationPage(proximityReminder: proximityReminder),
+            ));
+          },
+          child: Card(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                proximityReminder.phoneNumberName,
                 textAlign: TextAlign.center,
                 style: const TextStyle(
+                  fontWeight: FontWeight.bold,
                   color: Colors.black,
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10.0),
-              child: Text(
-                'Address: ${proximityReminder.latitude}',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.black,
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Text(
+                  'Phone: ${proximityReminder.phoneNumber}',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Text(
+                  'Address: ${proximityReminder.latitude}',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
                 ),
               ),
             ),
--- frontend/inrix_hack_22/lib/pages/location_page.dart
+++ frontend/inrix_hack_22/lib/pages/location_page.dart
@@ -2,30 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:inrix_hack_22/backend/geolocation.dart';
+import 'package:inrix_hack_22/models/proximity_reminder.dart';
 
 class LocationPage extends StatefulWidget {
-  const LocationPage({Key? key}) : super(key: key);
+  const LocationPage({Key? key, required this.proximityReminder})
+      : super(key: key);
+  final ProximityReminder proximityReminder;
 
   @override
   State<LocationPage> createState() => _LocationPageState();
 }
 
 class _LocationPageState extends State<LocationPage> {
-  late Position _currentPosition = Position(
-      longitude: 1,
-      latitude: 1,
-      timestamp: DateTime.now(),
-      accuracy: 1,
-      altitude: 1,
-      heading: 1,
-      speed: 1,
-      speedAccuracy: 1);
-
   @override
   void initState() {
     super.initState();
-
-    //_getCurrentPosition();
   }
 
   @override
@@ -37,42 +28,15 @@ class _LocationPageState extends State<LocationPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('LAT: ${_currentPosition.latitude}'),
-              Text('LNG: ${_currentPosition.longitude}'),
-              //Text('ADDRESS: ${_currentAddress ?? ""}'),
-              const SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: _getCurrentPosition,
-                // change the text
-                child: const Text("Get Current Location"),
-              )
+              // add map here
+              Text('Address: ${widget.proximityReminder.address}'),
+              Text('Phone Number: ${widget.proximityReminder.phoneNumber}'),
+              Text(
+                  'Phone Number Name: ${widget.proximityReminder.phoneNumberName}'),
             ],
           ),
         ),
       ),
     );
   }
-
-  void _getCurrentPosition() async {
-    Position currentPosition = await determinePosition();
-
-    setState(() {
-      _currentPosition = currentPosition;
-    });
-  }
-
-/*
-  Future<void> _getAddressFromLatLng(Position position) async {
-    await placemarkFromCoordinates(
-            _currentPosition!.latitude, _currentPosition!.longitude)
-        .then((List<Placemark> placemarks) {
-      Placemark place = placemarks[0];
-      setState(() {
-        _currentAddress =
-            '${place.street}, ${place.subLocality}, ${place.subAdministrativeArea}, ${place.postalCode}';
-      });
-    }).catchError((e) {
-      debugPrint(e);
-    });
-  }*/
 }
