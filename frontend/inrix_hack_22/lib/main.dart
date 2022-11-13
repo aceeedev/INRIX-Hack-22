e14cf28102a1b806b38f9975d9b1ff9238f024a6
Andrew Collins <56804602+aceeedev@users.noreply.github.com>
2022-11-12 16:07:34 -0800

    Initial Geolocation Works

--- frontend/inrix_hack_22/lib/main.dart
+++ frontend/inrix_hack_22/lib/main.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:inrix_hack_22/app.dart';
 
 void main() {
-  stderr.writeln('starting application');
   runApp(const MyApp());
 }
