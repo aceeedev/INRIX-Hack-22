e0e219a1845dab3dfdf079c8532c111287e88320
Andrew Collins <56804602+aceeedev@users.noreply.github.com>
2022-11-13 02:10:06 -0800

    Styling Home Page

--- frontend/inrix_hack_22/lib/app.dart
+++ frontend/inrix_hack_22/lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inrix_hack_22/pages/home_page.dart';
+import 'package:inrix_hack_22/styles.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Proximity Reminder',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: createMaterialColor(const Color(0xff0E366A)),
       ),
       home: const HomePage(),
     );
